c35639d9447eb6bcb33072e37251994adf51ea66
start.vaadin.com <no-reply@vaadin.com>
2024-04-09 23:12:23 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -0,0 +1,22 @@
+package com.example.application.views.home;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Home")
+@Route(value = "home", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@Uses(Icon.class)
+public class HomeView extends Composite<VerticalLayout> {
+
+    public HomeView() {
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+    }
+}
